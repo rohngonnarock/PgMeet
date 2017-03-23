c7c9a723108669809b568df653a42d0f1d5a5843
rahul <rohngonnarock>
2017-03-23 19:56:05 +0530

    test commit

--- PgMeet/src/app/app.module.ts
+++ PgMeet/src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
+    MaterialModule,
     BrowserModule,
     FormsModule,
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [TestComponent]
 })
 export class AppModule { }
