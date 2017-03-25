d0c9ecdad95e310455829a68c6cbf6875ab1eaac
rahul <rohngonnarock>
2017-03-25 19:05:32 +0530

    travisfile update

--- before_script.sh
+++ before_script.sh
@@ -1,9 +1,10 @@
+echo before_script test scenerio
 echo apiKey $apiKey
 echo authDomain ${authDomain}
 echo EnvdatabaseURL $EnvdatabaseURL
 
-sed -i 's/EnvapiKey/$apiKey/g' src/app/config/config.ts
-sed -i 's/EnvauthDomain/${authDomain}/g' src/app/config/config.ts
-sed -i 's/EnvdatabaseURL/${databaseURL}/g' src/app/config/config.ts
-sed -i 's/EnvstorageBucket/${storageBucket}/g' src/app/config/config.ts
-sed -i 's/EnvmessagingSenderId/${messagingSenderId}/g' src/app/config/config.ts
+sed -ie 's/EnvapiKey/$apiKey/g' src/app/config/config.ts
+sed -ie 's/EnvauthDomain/${authDomain}/g' src/app/config/config.ts
+sed -ie 's/EnvdatabaseURL/${databaseURL}/g' src/app/config/config.ts
+sed -ie 's/EnvstorageBucket/${storageBucket}/g' src/app/config/config.ts
+sed -ie 's/EnvmessagingSenderId/${messagingSenderId}/g' src/app/config/config.ts
