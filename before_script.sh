d1e9a512fdb14514e2b57232abefd03c4d910393
rahul <rohngonnarock>
2017-03-25 18:57:18 +0530

    before sript udpated

--- before_script.sh
+++ before_script.sh
@@ -1,4 +1,8 @@
-sed -i 's/EnvapiKey/${apiKey}/g' src/app/config/config.ts
+echo apiKey $apiKey
+echo authDomain ${authDomain}
+echo EnvdatabaseURL $EnvdatabaseURL
+
+sed -i 's/EnvapiKey/$apiKey/g' src/app/config/config.ts
 sed -i 's/EnvauthDomain/${authDomain}/g' src/app/config/config.ts
 sed -i 's/EnvdatabaseURL/${databaseURL}/g' src/app/config/config.ts
 sed -i 's/EnvstorageBucket/${storageBucket}/g' src/app/config/config.ts
